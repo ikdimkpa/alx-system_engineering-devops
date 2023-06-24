248073e99f0c2a9a9a2b5dcbbd4ad7d6345fed05
Ikenna Dimkpa <ikennadimkpa@gmail.com>
2023-06-23 17:20:22 -0700

    1-install_a_package.pp_debugged

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,12 +1,6 @@
 # Install flask version 2.1.0 using puppet
 
-package { 'pip3':
-ensure => 'latest',
-provider => 'pip',
-}
-
 package { 'flask':
 ensure => '2.1.0',
 provider => 'pip',
-require => Package['pip3'],
 }

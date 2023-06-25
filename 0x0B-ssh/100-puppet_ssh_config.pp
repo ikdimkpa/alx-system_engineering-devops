f5117bfb494184a456113a28af5090f3c03e7189
Ikenna Dimkpa <ikennadimkpa@gmail.com>
2023-06-25 16:18:12 -0700

    0x0B-ssh_100-puppet_ssh_config.pp

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,12 @@
+# edit ssh config file for privatekey and passwd auth
+exec {'Turn off passwd auth':
+  path    => '/usr/bin:/bin',
+  command => 'echo "PasswordAuthentication no" >> /etc/ssh/ssh_config'
+  replace => true,
+}
+
+exec {'Declare identity file':
+  path    => '/usr/bin:/bin',
+  command => 'echo "IdentityFile ~/.ssh/school" >> /etc/ssh/ssh_config'
+  replace => true,
+}

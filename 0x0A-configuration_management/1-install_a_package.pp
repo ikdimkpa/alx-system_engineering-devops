67066a123c237d6fb7504cbfe25d43ff46bb9899
ikdimkpa <ikennadimkpa@gmail.com>
2023-07-24 06:16:16 -0700

    preparing to defer to cohort 12

--- 0x0A-configuration_management/1-install_a_package.pp
+++
@@ -1,6 +0,0 @@
-# Install flask version 2.1.0 using puppet
-
-package { 'flask':
-ensure   => '2.1.0',
-provider => 'pip3',
-}
--- 0x0A-configuration_management/2-execute_a_command.pp
+++
@@ -1,5 +0,0 @@
-# using puppet to create a manifest that kills a process.
-
-exec { 'pkill -f killmenow':
-path => 'usr/bin:/bin',
-}
